0056c8b451bb3ec19d639e3771cd683255c8e769
GiacomoVisin <killerboss1150@gmail.com>
2026-02-25 14:08:40 +0100

    task n5 bonus

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -26,6 +26,6 @@ const cityNameConcat = `${city} ${ cityName}`;
 console.log(cityNameConcat); // Output: "New York City"
 
 // 5. Tutto in maiscolo
-// Trasforma tutto in maiscuolo la stringa della variabile hello
-const upperCaseHello = '';
+// Trasforma tutto in maiuscolo la stringa della variabile hello
+const upperCaseHello = (hello.toUpperCase());
 console.log(upperCaseHello); // Output: "HELLO"
